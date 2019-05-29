0fd16024555118cf548649da8ff795f508d5a465
Codegnosis <paul@codegnosis.co.uk>
2019-05-29 14:38:10 +0100

    indentationification

--- public/js/WRKChainEventWatcher.proto.js
+++ public/js/WRKChainEventWatcher.proto.js
@@ -3,61 +3,60 @@ function WRKChainEventWatcher(_contractAddress,
                               _abi,
                               _wrkchainNetworkId) {
 
-    //let abi = _abi.replace(/\\(.)/mg, "$1");
+  this.web3js = null;
+  this.contractAddress = _contractAddress;
+  this.abi = _abi;
+  this.lastEvent = null;
+  this.wrkchainNetworkId = parseInt(_wrkchainNetworkId);
 
-    this.web3js = null;
-    this.contractAddress = _contractAddress;
-    this.abi = _abi;
-    this.lastEvent = null;
-    this.wrkchainNetworkId = parseInt(_wrkchainNetworkId);
+  let self = this;
 
-    let self = this;
+  this.web3js = new Web3(new Web3.providers.HttpProvider(_web3ProviderUrl));
 
-    this.web3js = new Web3(new Web3.providers.HttpProvider(_web3ProviderUrl));
-
-    this.wrkchainRootContract = new this.web3js.eth.Contract(this.abi, this.contractAddress);
-
+  this.wrkchainRootContract = new this.web3js.eth.Contract(this.abi,
+                                                           this.contractAddress
+                                                           );
 }
 
 WRKChainEventWatcher.prototype.getLatestRecordedHeader = function(_callback) {
 
-    let self = this;
+  let self = this;
 
-    this.getCurrentBlockNumber().then(blockNumber => {
+  this.getCurrentBlockNumber().then(blockNumber => {
 
-        let fromBlock = blockNumber -10;
-        if(fromBlock < 0) {
-          fromBlock = 0;
+    let fromBlock = blockNumber -10;
+    if(fromBlock < 0) {
+      fromBlock = 0;
+    }
+
+    self.wrkchainRootContract.getPastEvents('RecordHeader', {
+      filter: {_chainId: this.wrkchainNetworkId},
+      fromBlock: fromBlock,
+      toBlock: 'latest'
+    }, (error, events) => {
+      if(error) {
+        console.log("error", error);
+        if(self.lastEvent != null) {
+           _callback(true, self.lastEvent);
+        } else {
+           _callback(false, error);
         }
-
-        self.wrkchainRootContract.getPastEvents('RecordHeader', {
-            filter: {_chainId: this.wrkchainNetworkId},
-            fromBlock: fromBlock,
-            toBlock: 'latest'
-        }, (error, events) => {
-           if(error) {
-               console.log("error", error);
-               if(self.lastEvent != null) {
-                   _callback(true, self.lastEvent);
-               } else {
-                   _callback(false, error);
-               }
-           } else {
-               let latestEvent = events[events.length - 1]
-               self.lastEvent = latestEvent;
-               _callback(true, latestEvent);
-           }
-         });
-        return;
+      } else {
+        let latestEvent = events[events.length - 1]
+        self.lastEvent = latestEvent;
+        _callback(true, latestEvent);
+      }
     });
+    return;
+  });
 }
 
 WRKChainEventWatcher.prototype.getCurrentBlockNumber = async function () {
-    let blockNumber = await this.web3js.eth.getBlockNumber();
-    return blockNumber;
+  let blockNumber = await this.web3js.eth.getBlockNumber();
+  return blockNumber;
 }
 
 WRKChainEventWatcher.prototype.getChainId = async function () {
-    let blockNumber = await this.web3js.eth.getBlockNumber();
-    return blockNumber;
+  let blockNumber = await this.web3js.eth.getBlockNumber();
+  return blockNumber;
 }
--- public/js/WRKChainRoot.proto.js
+++ public/js/WRKChainRoot.proto.js
@@ -4,66 +4,60 @@ function WRKChainRoot(_wrkchainRootContractAddress,
                        _wrkchainRootAbi,
                        _wrkchainNetworkId) {
 
-    //let abi = _wrkchainRootAbi.replace(/\\(.)/mg, "$1");
+  this.web3jsMainchain = null;
+  this.contractAddress = _wrkchainRootContractAddress;
+  this.abi = _wrkchainRootAbi;
+  this.wrkchainNetworkId = parseInt(_wrkchainNetworkId);
 
-    this.web3jsMainchain = null;
-    this.contractAddress = _wrkchainRootContractAddress;
-    this.abi = _wrkchainRootAbi;
-    this.wrkchainNetworkId = parseInt(_wrkchainNetworkId);
+  let self = this;
 
-    let self = this;
+  this.web3jsMainchain = new Web3(new Web3.providers.HttpProvider(_mainchainWeb3ProviderUrl));
+  this.web3jsWrkchain = new Web3(new Web3.providers.HttpProvider(_wrkchainWeb3ProviderUrl));
 
-    this.web3jsMainchain = new Web3(new Web3.providers.HttpProvider(_mainchainWeb3ProviderUrl));
-    this.web3jsWrkchain = new Web3(new Web3.providers.HttpProvider(_wrkchainWeb3ProviderUrl));
-
-    this.wrkchainRootContract = new this.web3jsMainchain.eth.Contract(this.abi, this.contractAddress);
-
+  this.wrkchainRootContract = new this.web3jsMainchain.eth.Contract(this.abi, this.contractAddress);
 }
 
 WRKChainRoot.prototype.validateBlock = function(_block_num, _callback) {
 
-    let self = this;
-    this.getWrkchainBlock(_block_num).then(wrkchain_block => {
-        this.runBlockHeaderFromRoot(_block_num).then(wrkchain_root_data => {
-            _callback(wrkchain_block, wrkchain_root_data);
-            return;
-        });
-        return;
+  let self = this;
+  this.getWrkchainBlock(_block_num).then(wrkchain_block => {
+    this.runBlockHeaderFromRoot(_block_num).then(wrkchain_root_data => {
+      _callback(wrkchain_block, wrkchain_root_data);
+      return;
     });
+    return;
+  });
 }
 
 WRKChainRoot.prototype.getWrkchainBlock = async function(_block_num) {
-    let wrkchain_block = await this.web3jsWrkchain.eth.getBlock(_block_num);
-    return wrkchain_block;
+  let wrkchain_block = await this.web3jsWrkchain.eth.getBlock(_block_num);
+  return wrkchain_block;
 }
 
 WRKChainRoot.prototype.runBlockHeaderFromRoot = async function (_block_num) {
-    let wrkchain_root_data = await this.getBlockHeaderFromRoot(_block_num);
-    return wrkchain_root_data;
+  let wrkchain_root_data = await this.getBlockHeaderFromRoot(_block_num);
+  return wrkchain_root_data;
 }
 
 WRKChainRoot.prototype.getBlockHeaderFromRoot = function (_block_num) {
-    let self = this;
-    return new Promise(resolve => {
-
-        self.wrkchainRootContract.getPastEvents('RecordHeader', {
-            filter: {_chainId: this.wrkchainNetworkId, _height: _block_num},
-            fromBlock: 0,
-            toBlock: 'latest'
-        }, (error, events) => {
-           if(error) {
-               console.log("error", error);
-
-           } else {
-               let latestEvent = events[0]
-               resolve(latestEvent);
-           }
-         });
+  let self = this;
+  return new Promise(resolve => {
+    self.wrkchainRootContract.getPastEvents('RecordHeader', {
+      filter: {_chainId: this.wrkchainNetworkId, _height: _block_num},
+      fromBlock: 0,
+      toBlock: 'latest'
+    }, (error, events) => {
+      if(error) {
+         console.log("error", error);
+      } else {
+         let latestEvent = events[0]
+         resolve(latestEvent);
+      }
     });
+  });
 }
 
 WRKChainRoot.prototype.getCurrentBlockNumber = async function () {
-    let blockNumber = await this.web3js.eth.getBlockNumber();
-    return blockNumber;
+  let blockNumber = await this.web3js.eth.getBlockNumber();
+  return blockNumber;
 }
-
